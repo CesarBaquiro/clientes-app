893cc3e0f43ce6db0dc4db45f8cae346af32fb12
CesarBaquiro <cmbm.kf@gmail.com>
2023-10-03 16:29:27 -0500

    Creacion hasta lista de clientes con servicio

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,39 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    nombre: 'Andres',
+    apellido: 'Guzman',
+    email: 'guzman@gmail.com',
+    createAt: '2011-10-1',
+  },
+  {
+    id: 2,
+    nombre: 'Camilo',
+    apellido: 'Gonzales',
+    email: 'gusdasan@gmail.com',
+    createAt: '2021-10-28',
+  },
+  {
+    id: 3,
+    nombre: 'Fani',
+    apellido: 'Caman',
+    email: 'gCn@gmail.com',
+    createAt: '2010-10-28',
+  },
+  {
+    id: 4,
+    nombre: 'Bander',
+    apellido: 'Poran',
+    email: 'gudadad@gmail.com',
+    createAt: '2011-10-28',
+  },
+  {
+    id: 5,
+    nombre: 'Campan',
+    apellido: 'Bobaan',
+    email: 'Mann@gmail.com',
+    createAt: '2011-10-11',
+  },
+];
