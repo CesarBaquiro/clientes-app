37d60289210b7cc39c3795aa6ccf728409b12815
CesarBaquiro <cmbm.kf@gmail.com>
2023-10-07 06:19:13 -0500

    Se arreglo el bug del metodo put

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './clientes/form/form.component';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: ' ', redirectTo: '/clientes', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/form', component: FormComponent },
+  { path: 'clientes/form/:id', component: FormComponent },
+];
 
 @NgModule({
   declarations: [
@@ -13,12 +25,16 @@ import { ClientesComponent } from './clientes/clientes.component';
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import { of, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
-  constructor() {}
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) {}
 
-  getClientes(): Cliente[] {
-    return CLIENTES;
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders,
+    });
+  }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  }
+
+  update(id: number, clienteData: Cliente): Observable<Cliente> {
+    const url = `${this.urlEndPoint}/${id}`;
+    return this.http.put<Cliente>(url, clienteData);
   }
 }
